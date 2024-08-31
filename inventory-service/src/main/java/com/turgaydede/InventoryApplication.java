d2866add1ce6bdeb24a6b66c9e852bca2e7f9a16
tdede <turgay.dede28@hotmail.com>
2024-08-31 23:16:52 +0300

    mock data init

--- inventory-service/src/main/java/com/turgaydede/InventoryApplication.java
+++ inventory-service/src/main/java/com/turgaydede/InventoryApplication.java
@@ -1,16 +1,25 @@
 package com.turgaydede;
 
+import com.turgaydede.command.CreateProductCommand;
 import com.turgaydede.command.api.errorhandling.InventoryServiceEventsErrorHandler;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.UUID;
+
 @SpringBootApplication
-public class InventoryApplication {
+public class InventoryApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
     }
+
+    @Autowired
+    private CommandGateway commandGateway;
 
     @Autowired
     public void configure(EventProcessingConfigurer configurer) {
@@ -22,4 +31,19 @@ public class InventoryApplication {
 //                configuration -> PropagatingErrorHandler.instance());
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        List<CreateProductCommand> products = List.of(
+                new CreateProductCommand("8088c6c8-182e-4417-87cc-ea00ef35b1ec", "İphone 11", 25000.0, 14),
+                new CreateProductCommand("30f12d34-e4d7-4cc2-aa0d-ca3fca7c07ab", "Samsung Galaxy S21", 22000.0, 20),
+                new CreateProductCommand("60b3efe8-48f7-48f3-a14e-bf2befbac79d", "Huawei P40", 18000.0, 10),
+                new CreateProductCommand("cf675c20-1820-4e4f-b90a-bfb4b51f8c81", "OnePlus 9", 21000.0, 15),
+                new CreateProductCommand("b49d7e56-1d86-43d0-84e7-58fc49b55ca4", "Xiaomi Mi 11", 20000.0, 12)
+        );
+
+
+        for (CreateProductCommand product : products) {
+            commandGateway.send(product);
+        }
+    }
 }
--- user-service/src/main/java/com/turgaydede/UserApplication.java
+++ user-service/src/main/java/com/turgaydede/UserApplication.java
@@ -1,11 +1,64 @@
 package com.turgaydede;
 
+import com.turgaydede.data.CreditCard;
+import com.turgaydede.data.UserEntity;
+import com.turgaydede.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class UserApplication {
+public class UserApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        UserEntity user = UserEntity.builder()
+                .id("a77d2ab3-fb27-4341-8a97-7cdb85f7f4c1")
+                .username("john_doe")
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .password("SecurePassword123")
+                .build();
+
+        CreditCard card1 = CreditCard.builder()
+                .id("a4e22f52-a7d6-4226-b237-527570ddb1ec")
+                .name("John Doe")
+                .cardNumber("1234567812345678")
+                .validUntilMonth(12)
+                .validUntilYear(2025)
+                .cvv(123)
+                .user(user)
+                .build();
+
+        CreditCard card2 = CreditCard.builder()
+                .id("a4e22f52-a7d6-4226-b237-527570ddb2ec")
+                .name("John Doe")
+                .cardNumber("8765432187654321")
+                .validUntilMonth(7)
+                .validUntilYear(2024)
+                .cvv(456)
+                .user(user)
+                .build();
+
+        List<CreditCard> cardDetails = List.of(card1, card2);
+
+        user.setCardDetails(cardDetails);
+
+
+
+        userRepository.save(user);
+
+
     }
 }
