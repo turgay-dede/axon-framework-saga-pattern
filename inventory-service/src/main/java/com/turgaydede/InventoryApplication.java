06b79997859622db41c163ec083d327e262057e8
tdede <turgay.dede28@hotmail.com>
2024-08-31 13:47:01 +0300

    refactor

--- inventory-service/src/main/java/com/turgaydede/InventoryApplication.java
+++ inventory-service/src/main/java/com/turgaydede/InventoryApplication.java
@@ -2,12 +2,9 @@ package com.turgaydede;
 
 import com.turgaydede.command.api.errorhandling.InventoryServiceEventsErrorHandler;
 import org.axonframework.config.EventProcessingConfigurer;
-import org.axonframework.eventhandling.TrackingEventProcessorConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class InventoryApplication {
